9075069761cd9aad9267b0d6c58e09bd06139ef8
sverkip <yonatansverdlov276@gmail.com>
2022-07-28 13:11:59 +0300

    Supporting all datasets. And fixing bugs.

--- yonathan/create_dataset.py
+++ yonathan/create_dataset.py
@@ -1,100 +1,82 @@
-import os
 import sys
-import skimage.transform
-from skimage import color
-import numpy as np
-import pickle
-from PIL import Image
 import datetime
-import argparse
 # for copying the generating script
 import __main__ as mainmod
 import shutil
-import torchvision
-from torchvision.utils import save_image
 from torchvision import transforms
-import torch
-from skimage import io
+from multiprocessing import Pool
 from Create_dataset_utils import *
 from parser import *
-from multiprocessing import Pool
-from temp import *
+from Raw_data_loaders import *
 
 #TODO-assert nclasses is the correct one.
-def gen_sample(sample_id:int, is_train:bool, aug_data:transforms, OmniglotLoader:OmniglotDataLoader, nclasses:int, CHAR_SIZE:int, IMAGE_SIZE:list, example:example_class, augment_sample,num_examples_per_character:int)->Sample:
+def gen_sample(parser:argparse, sample_id:int, is_train:bool, aug_data:transforms, dataloader:DataSet, example:ExampleClass, augment_sample:bool)->Sample:
     """
-    #
-    Creates a single sample including image, label_task, label_all, label_existence, query_index.
-    #
-    :param sample_id: The sample id in all samples.
-    :param is_train: If the sample is for training.
-    :param aug_data:
-    :param OmniglotLoader:
-    :param nclasses: The augmentation transform.
-    :param CHAR_SIZE: The character size.
-    :param IMAGE_SIZE: The desired image size.
-    :param example: The character example we sampled.
-    :param augment_sample: Whether to augment the sample or not.
-    :param num_examples_per_character: The num of examples per language
+    Creates a single sample including image, label_task, label_all, label_existence, query_index
+    Args:
+        parser: The option parser.
+        sample_id: The sample id in all samples.
+        is_train: If the sample is for training.
+        aug_data: The data augmentation transform.
+        dataloader: The raw data loader.
+        example: The example containing the selected characters.
+        augment_sample: Whether to augment the sample, true for train otherwise false.
+
+    Returns: A sample.
     """
     # start by creating the image background(all black)
-    image = 0 * np.ones(IMAGE_SIZE, dtype=np.float32)
+    image = 0 * np.ones(parser.image_size, dtype=np.float32)
     infos = [] #Stores all the information about the characters.
-    for char in example.chars: # Iterate for each chosen character.
-     image,info = AddCharacterToExistingImage(OmniglotLoader,image ,char,CHAR_SIZE,num_examples_per_character) # Adding the character to the image.
+    for char in example.chars: # Iterate over each chosen character.
+     image,info = AddCharacterToExistingImage(dataloader, image, char) # Adding the character to the image.
      infos.append(info) # Adding to the info about the characters.
     #Making label_existence flag.
-    label_existence = Get_label_existence(infos,nclasses) 
+    label_existence = Get_label_existence(infos, dataloader.nclasses)
     # the characters in order as seen in the image
     label_ordered = Get_label_ordered(infos)
     # instruction and task label
-    label_task, flag, query_index = Get_label_task(example, infos,label_ordered,nclasses)
+    label_task, flag = Get_label_task(example, infos, label_ordered, dataloader.nclasses)
     # even for grayscale images, store them as 3 channels RGB like
-    if len(image.shape) == 2:
-        image = image[:, :, np.newaxis]
+    if image.shape[0] == 1:
+        image =  np.concatenate((image, image, image), axis=0)
     # Making RGB.
-    image = 255 * np.concatenate((image, image, image), axis=2)
+    image = image * 255
     image = image.astype(np.uint8)
     # Doing data augmentation
     if is_train and augment_sample:
         # augment
-        Data_augment = DataAugmentClass(image, label_existence, aug_data)
-        image = Data_augment.get_batch_base()
+        data_augment = DataAugmentClass(image, label_existence, aug_data)
+        image = data_augment.get_batch_base()
     # Storing the needed information about the sample.
-    sample = Sample(infos, image, sample_id, label_existence, label_ordered, query_index, label_task, flag, is_train)
+    sample = Sample(infos, image, sample_id, label_existence, label_ordered, example.query_part_id, label_task, flag, is_train)
     return sample # Returning the sample we are going to store.
 
-def gen_samples(parser,Omniglot_loader, job_id, range_start, range_stop, examples, storage_dir,ds_type, nclasses, job_chunk_size, augment_sample, folder_split, folder_size)->None:
+def gen_samples(parser:argparse, dataloader:DataSet, job_id:int, range_start:int, range_stop:int, examples:list, storage_dir:str, ds_type:str, augment_sample:bool)->None:
     """
-    #
-    Generates and stored samples, by calling to create_sample and store_sample_disk_pytorch
-    #
-    :param parser: The creation parser.
-    :param Omniglot_loader: The data loader.
-    :param job_id: The job id.
-    :param range_start: The range start. 
-    :param range_stop: The range stop.
-    :param examples: The examples.
-    :param storage_dir: The directory to store in.
-    :param ds_type: The ds_type.
-    :param nclasses: The number of classes.
-    :param job_chunk_size: The number of samples each job processes.
-    :param augment_sample: Whether to augment the sample.
-    :param folder_split: Whether to split the folder.
-    :param folder_size: The folder size.
+    Generates and stored samples, by calling to create_sample and store_sample_disk_pytorch.
+    Args:
+        parser: The option parser.
+        dataloader: The raw data loader.
+        job_id: The job id.
+        range_start: The range start in the job.
+        range_stop: The range stop of the job.
+        examples: The chosen examples.
+        storage_dir: The storage directory
+        ds_type: The data-set type.
+        augment_sample: Whether to augment the sample.
+
     """
-    CHAR_SIZE = parser.CHAR_SIZE # The character size.
-    IMAGE_SIZE = parser.IMAGE_SIZE # The image size.
+    image_size = parser.image_size # The image size.
     aug_data = None # The augmentation transform.
     is_train = ds_type == 'train' # Whether the dataset is of type train.
     if is_train: # Creating the augmentation transform.
         if augment_sample:
             # create a separate augmentation per job since we always update aug_data.aug_seed
-            aug_data = get_aug_data(IMAGE_SIZE)
+            aug_data = GetAugData(image_size)
             aug_data.aug_seed = range_start
             aug_data.augment = True
     # divide the job into several smaller parts and run them sequentially
-    ranges = np.arange(range_start, range_stop, job_chunk_size)
+    ranges = np.arange(range_start, range_stop, parser.job_chunk_size)
     if ranges[-1] != range_stop:
         ranges = ranges.tolist()
         ranges.append(range_stop)
@@ -105,34 +87,25 @@ def gen_samples(parser,Omniglot_loader, job_id, range_start, range_stop, example
         print('%s: job %d. processing: %s-%d-%d' % (datetime.datetime.now(), job_id, ds_type, range_start, range_stop - 1))
         cur_samples_dir = os.path.join(storage_dir, ds_type)  # Making the path.
         if not os.path.exists(cur_samples_dir): # creating the train/test/val paths is needed.
-            os.makedirs(cur_samples_dir, exist_ok=True)
+            os.makedirs(cur_samples_dir)
         print('%s: storing in: %s' % (datetime.datetime.now(), cur_samples_dir))
         sys.stdout.flush()
         for samid in range(range_start, range_stop):
             # Generating the samples.
-            sample = gen_sample(samid, is_train, aug_data, Omniglot_loader, nclasses, CHAR_SIZE, IMAGE_SIZE, examples[rel_id], augment_sample,Omniglot_loader.num_examples_per_character)
+            sample = gen_sample(parser, samid, is_train, aug_data, dataloader, examples[rel_id], augment_sample)
             if sample is None:
                 continue
             # Stores the samples.
-            store_sample_disk(sample, cur_samples_dir, folder_split, folder_size)
+            store_sample_disk(sample, cur_samples_dir, parser.folder_split, parser.folder_size)
             rel_id += 1
     print('%s: Done' % (datetime.datetime.now()))
 
-
-
-
 def main(language_list:list)->None:
-  #  cmd_args = parser.parse_args()
-    Omniglot_loader = OmniglotDataLoader(language_list) # Getting the raw data.
-    parser = Get_parser()
-
-
-  #  dictionary = create_dict(parser.path_data_raw)  # The dictionary assigning for each language its number of characters.
-  #  dictionary = dict(sorted(dictionary.items(), key=lambda item: item[1]))
-  #  Omniglot_loader = EmnistLoader(parser.store_folder)
     # Getting the option parser.
+    parser = Get_parser()
+    raw_data_set = DataSet(data_dir = '/home/sverkip/data/Create_dataset_adapting_to_all_datasets/data',dataset = 'emnist',raw_data_source=parser.path_data_raw,language_list=[49]) # Getting the raw data.
+    parser.image_size = (raw_data_set.nchannels,*parser.image_size)
     njobs = parser.threads # The number of threads.
-    dictionary = create_dict(parser.path_data_raw) # The dictionary assigning for each language its number of characters.
     num_rows_in_the_image   = parser.num_rows_in_the_image      # The number of rows in the image.
     obj_per_row = parser.nchars_per_row            # The number of characters in the row.
     num_chars_per_image = parser.nchars_per_row * parser.num_rows_in_the_image       # The number of characters in the image.
@@ -140,7 +113,8 @@ def main(language_list:list)->None:
     nsamples_train = parser.nsamples_train         # The number of train samples we desire to create.
     nsamples_val = parser.nsamples_val             # The number of validation samples we desire to create.
     ngenerate = parser.ngenerate                   # The number of queries to create for each sample.
-    nclasses = Omniglot_loader.nclasses            # The number of classes in the dataset.
+    nclasses = raw_data_set.nclasses            # The number of classes in the dataset.
+    parser.letter_size = raw_data_set.letter_size
     single_feat_to_generate = parser.single_feat_to_generate # Whether to create single query per sample.
     valid_classes = Get_valid_classes(parser, nclasses)  # The valid classes, relevant for mnist.
     ndirections = parser.ndirections               # The number of directions we want to query about.
@@ -148,8 +122,11 @@ def main(language_list:list)->None:
     job_chunk_size = parser.job_chunk_size # The number of samples each job should handle.
     augment_sample = parser.augment_sample
     # Use multiprocessing on this machine
-    local_multiprocess = njobs > 1 # NOT SURE-ASK LIAV.
+    local_multiprocess = njobs > 1
     image_ids = set()
+    test_chars_list = []
+    valid_pairs = None
+    ntest_strings = parser.ntest_strings
 
     # each 'job' processes several chunks. Each chunk is of 'storage_batch_size' samples
     conf_data_fname, storage_dir = Get_data_dir(parser, parser.store_folder,language_list) # Get the storage dir for the data and for the conf file.
@@ -157,7 +134,6 @@ def main(language_list:list)->None:
      avail_adj_types = range(ndirections)
     else:
      avail_adj_types = [0]
-
     ds_types = ['test', 'train'] # The dataset types.
     nexamples_vec = [nsamples_test, nsamples_train]
     if generalize: # if we want also the CG dataset we add its name to the ds_type and the number of needed samples.
@@ -165,10 +141,8 @@ def main(language_list:list)->None:
         ds_types.append('val')
     else:
         nsamples_val = 0
-
     if generalize: # If we want the CG dataset, we choose the excluded combinations and pears.
-        valid_pairs,test_chars_list = Get_valid_pairs_for_the_combenatorial_test(parser, nclasses,valid_classes,num_chars_per_image,obj_per_row  )
-        ntest_strings = 1
+        valid_pairs,test_chars_list = Get_valid_pairs_for_the_combinatorial_test(parser, nclasses, valid_classes, num_chars_per_image)
     # Iterating over all dataset types, and its number of desired number of samples.
     for k, (ds_type, cur_nexamples) in enumerate(zip(ds_types, nexamples_vec)):
         prng = np.random.RandomState(k)
@@ -181,22 +155,22 @@ def main(language_list:list)->None:
                 sample_chars = Get_sample_chars(prng, valid_pairs, is_test, valid_classes, num_chars_per_image, ntest_strings, test_chars_list)
             else:
                 # Otherwise we choose from all the valid classes, without replacement the desired number of characters.
-                sample_chars = prng.choice(valid_classes,sample_nchars,replace=False)
+                sample_chars = prng.choice(valid_classes, num_chars_per_image, replace=False)
             image_id = []
             label_ids = []
             # For each character, sample an id in the number of characters with the same label.
             for _ in sample_chars:
-                label_id = prng.randint(0, Omniglot_loader.num_examples_per_character ) # Choose a possible label_id.
+                label_id = prng.randint(0, raw_data_set.num_examples_per_character) # Choose a possible label_id.
                 image_id.append(label_id)
                 label_ids.append(label_id)
             image_id_hash = str(image_id)
-            if image_id_hash in image_ids: # ASK LIAV
+            if image_id_hash in image_ids:
                 continue
             image_ids.add(image_id_hash)
             # place the chars on the image
             chars = [] # The augmented characters.
             for samplei in range(num_chars_per_image): #For each chosen character, we augment it and transform it.
-                char = CharcterTransforms(prng, parser.CHAR_SIZE,label_ids,samplei,sample_chars,num_rows_in_the_image ,obj_per_row,parser.IMAGE_SIZE,num_chars_per_image)
+                char = CharacterTransforms(parser, prng, label_ids, samplei, sample_chars)
                 chars.append(char)
             print(i)
             #TODO - ASSERT WHAT WE WANT HERE.
@@ -217,7 +191,7 @@ def main(language_list:list)->None:
 
         print('total of %d examples' % cur_nexamples) # print the number of sampled examples.
         # divide all the examples across several jobs. Each job generates samples from examples
-        cur_njobs = min(njobs, np.ceil((cur_nexamples) / job_chunk_size).astype(int)) # The needed number of jobs.
+        cur_njobs = min(njobs, np.ceil(cur_nexamples / job_chunk_size).astype(int)) # The needed number of jobs.
         ranges = np.linspace(0, cur_nexamples, cur_njobs + 1).astype(int)
         # in case there are fewer ranges than jobs
         ranges = np.unique(ranges)
@@ -228,25 +202,25 @@ def main(language_list:list)->None:
             range_start = ranges[job_id]
             range_stop = ranges[job_id + 1]
             # Preparing the arguments for the generation.
-            args = (parser, Omniglot_loader, job_id, range_start, range_stop, examples[range_start:range_stop], storage_dir, ds_type, nclasses, job_chunk_size,   augment_sample,  parser.folder_split, parser.folder_size)
+            args = (parser, raw_data_set, job_id, range_start, range_stop, examples[range_start:range_stop], storage_dir, ds_type, augment_sample)
             all_args.append(args)
             if not local_multiprocess:
              gen_samples(*args) # Calling the generation function.
         if local_multiprocess:
-            with Pool(cur_njobs) as p:
-                p.starmap(gen_samples, all_args)#  Calling the generation function.
+            with Pool(cur_njobs) as process:
+                process.starmap(gen_samples, all_args)#  Calling the generation function.
 
     print('done') # Done creating and storing the samples.
     # store the dataset's properties.
     with open(conf_data_fname, "wb") as new_data_file:
-        pickle.dump((nsamples_train, nsamples_test, nsamples_val, nclasses,  parser.CHAR_SIZE, parser.IMAGE_SIZE, num_rows_in_the_image, obj_per_row, num_chars_per_image,ndirections, valid_classes), new_data_file)
-    #TODO -SAVE ALSO THE UTILS.
+        pickle.dump((nsamples_train, nsamples_test, nsamples_val, nclasses,  parser.letter_size, parser.image_size, num_rows_in_the_image, obj_per_row, num_chars_per_image,ndirections, valid_classes), new_data_file)
+    #TBD - SAVE ALSO THE UTILS.
     # copy the generating script
     script_fname = mainmod.__file__
     shutil.copy(script_fname, storage_dir)
 # %%
 if __name__ == "__main__":
   #   tasks = [[27,5]]
-     tasks = [ [27,5,42,18,33] ]
+     tasks = [ [17] ]
      for task in tasks:
-      ret = main(task)
+       main(task)
