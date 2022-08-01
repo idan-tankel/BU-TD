c305a446a76c8944d46ec4f15c2463a9f58fb944
tankwell <idan.tankel@weizmann.ac.il>
2022-08-01 12:52:50 +0300

    change the name of the datasety

--- yonathan/create_dataset.py
+++ yonathan/create_dataset.py
@@ -103,7 +103,7 @@ def gen_samples(parser:argparse, dataloader:DataSet, job_id:int, range_start:int
 def main(language_list:list)->None:
     # Getting the option parser.
     parser = Get_parser()
-    raw_data_set = DataSet(data_dir = '/home/sverkip/data/Create_dataset_adapting_to_all_datasets/data',dataset = 'emnist',raw_data_source=parser.path_data_raw,language_list=[49]) # Getting the raw data.
+    raw_data_set = DataSet(data_dir = '../data',dataset = 'emnist',raw_data_source=parser.path_data_raw,language_list=[49]) # Getting the raw data.
     parser.image_size = (raw_data_set.nchannels,*parser.image_size)
     njobs = parser.threads # The number of threads.
     num_rows_in_the_image   = parser.num_rows_in_the_image      # The number of rows in the image.
