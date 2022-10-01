3640ca90027c0ff68c3b8316a433c8f3a105b1dd
yonatansverdlov <yonatansverdlov276@gmail.com>
2022-10-01 11:00:25 +0300

    Making all code in one folder

--- yonathan/Recognicion/code/create_dataset/create_dataset.py
+++ yonathan/Recognicion/code/create_dataset/create_dataset.py
@@ -15,6 +15,6 @@ def main_kmnist():
     main(ds_type = DsType.Kmnist)
 
 #main_Omniglot([[12],[13],[15],[20]])
-main_Omniglot([[23], [24], [27], [28], [29], [32], [33], [34], [36], [37], [38],[40],[42],[43],[44], [46],[47],[49]])
+main_Omniglot([[14],[39]])
 
 
