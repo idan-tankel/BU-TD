984aeef910c5abfc658101fbeffc81e61fb0268e
tankwell <idan.tankel@weizmann.ac.il>
2022-10-23 16:50:28 +0300

    a patch on get_dataset file

--- yonathan/Recognicion/code/supp/get_dataset.py
+++ yonathan/Recognicion/code/supp/get_dataset.py
@@ -6,10 +6,11 @@ import sys
 from torch.utils.data import DataLoader
 
 from supp.Dataset_and_model_type_specification import DsType, Flag
-from supp.general_functions import preprocess
-from supp.training_functions import DatasetInfo
+from supp.datasets import DatasetAllDataSetTypesAll as dataset
 
-sys.path.append(r'/home/sverkip/data/BU-TD/yonathan/Recognicion/code/create_dataset')
+
+# TODO - GET RIT OF THIS.
+sys.path.append(r'/home/idanta/BU-TD/yonathan/Recognicion/code/create_dataset')
 
 
 def get_dataset_for_spatial_realtions(opts: argparse, data_fname: str, lang_idx: int, direction: int) -> list:
@@ -25,14 +26,12 @@ def get_dataset_for_spatial_realtions(opts: argparse, data_fname: str, lang_idx:
     (`tuple`): train_dl, test_dl, val_dl, train_ds, test_ds, val_ds
 
     """
-    if opts.ds_type is DsType.Omniglot and opts.model_flag is Flag.ZF:
-        from supp.datasets import OmniglotDataset as dataset
-    if (opts.ds_type is DsType.Emnist or opts.ds_type is DsType.FashionMnist) and opts.model_flag is not Flag.NOFLAG:
-        from supp.datasets import EmnistDataSet as dataset
-    if (opts.ds_type is DsType.Emnist or opts.ds_type is DsType.FashionMnist) and opts.model_flag is Flag.NOFLAG:
-        from supp.datasets import EmnistDataSetAll as dataset
 
-    use_val = opts.generalize
+    if opts.model_flag is not Flag.NOFLAG:
+        from supp.datasets import DatasetAllDataSetTypes as dataset
+    elif opts.model_flag is Flag.NOFLAG:
+        from supp.datasets import DatasetAllDataSetTypesAll as dataset
+
     path_fname = os.path.join(data_fname, 'MetaData')
     # Opening the conf file and retrieve number of samples, img shape,number of objects per image.
     with open(path_fname, "rb") as new_data_file:
@@ -49,15 +48,13 @@ def get_dataset_for_spatial_realtions(opts: argparse, data_fname: str, lang_idx:
     obj_per_col = MetaData.parser.num_rows_in_the_image
     # Creating the data-sets.
     if opts.ds_type is DsType.Omniglot:
-        train_ds = dataset(os.path.join(data_fname, 'train'), opts, lang_idx, direction, nsamples_train, obj_per_row,
-                           obj_per_col)
-        test_ds = dataset(os.path.join(data_fname, 'test'), opts, lang_idx, direction, nsamples_test, obj_per_row,
-                          obj_per_col)
+        arg_and_head_index = lang_idx
     else:
-        # TODO - OMIT KEYBOARD DEFAULTS.
-        train_ds = dataset(os.path.join(data_fname, 'train'), opts, direction, True, nsamples_train, obj_per_row, obj_per_col)
-        test_ds = dataset(os.path.join(data_fname, 'test'), opts, direction, False, nsamples_test, obj_per_row, obj_per_col)
-    # If normalize_image is True the mean of the dataset is subtracted from every image.
+        arg_and_head_index = 0
+    train_ds = dataset(root=os.path.join(data_fname, 'train'), opts=opts, arg_and_head_index = arg_and_head_index, direction=direction,
+                       is_train=True, nexamples=nsamples_train, obj_per_row=obj_per_row, obj_per_col=obj_per_col)
+    test_ds = dataset(root=os.path.join(data_fname, 'test'), opts=opts, arg_and_head_index=arg_and_head_index, direction=direction,
+                      is_train=False, nexamples=nsamples_test, obj_per_row=obj_per_row, obj_per_col=obj_per_col)
     batch_size = opts.bs
 
     # Creating the data-loaders.
@@ -68,19 +65,11 @@ def get_dataset_for_spatial_realtions(opts: argparse, data_fname: str, lang_idx:
     nbatches_train = len(train_dl)
     nbatches_test = len(test_dl)
     nbatches_val = 0
-    train_dataset = WrappedDataLoader(train_dl, preprocess)
-    test_dataset = WrappedDataLoader(test_dl, preprocess)
-    the_train_dataset = DatasetInfo(True, train_dataset, nbatches_train, 'Train', opts.checkpoints_per_epoch)
-    the_test_dataset = DatasetInfo(False, test_dataset, nbatches_test, 'Test', 1)
-    the_datasets = [the_train_dataset, the_test_dataset]
-    #
-    if use_val:
+
+    if os.path.exists(os.path.join(data_fname, 'val')):
         val_ds = dataset(os.path.join(data_fname, 'val'), opts, direction,False, nsamples_val, obj_per_row, obj_per_col)
         val_dl = DataLoader(val_ds, batch_size=batch_size, num_workers=opts.workers, shuffle=False, pin_memory=True)
         nbatches_val = len(val_dl)
-        val_dataset = WrappedDataLoader(val_dl, preprocess)
-        the_val_dataset = DatasetInfo(False, val_dataset, nbatches_val, 'Val', 1)
-        the_datasets.append(the_val_dataset)
     else:
         val_ds = None
     # Storing tht parameters into the Parser
