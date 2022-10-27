fixed accuracy calculation add model logs to wandb

yonathan/Recognicion/code/pytorch-lightning/trainer.py
@@ -1,22 +1,25 @@
 import sys
-from types import NoneType
 sys.path.append(r'/home/idanta/BU-TD/yonathan/Recognicion/code/')
-import git
-from supp.create_model import create_model
-from Configs.Config import Config
-from supp.Dataset_and_model_type_specification import Flag
-import torch.nn as nn
-from Checkpoint_model_definition import CheckpointSaver, ModelWrapped
-from pathlib import Path
-import os
-from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks import ModelCheckpoint
-from supp.get_dataset import get_dataset_for_spatial_realtions
-from supp.Parser import GetParser
+
 import pytorch_lightning as pl
+from supp.Parser import GetParser
+from supp.get_dataset import get_dataset_for_spatial_realtions
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
+import os
+from pathlib import Path
+from Checkpoint_model_definition import CheckpointSaver, ModelWrapped
+import torch.nn as nn
+from supp.Dataset_and_model_type_specification import Flag
+from Configs.Config import Config
+from supp.create_model import create_model
+import git
+import sys
+from types import NoneType
 # TODO write an own import function using imp
-
-
+git_repo = git.Repo(__file__, search_parent_directories=True)
+git_root = git_repo.working_dir
+sys.path.append(str(git_root))
 
 class Training_flag:
     def __init__(self, train_all_model: bool, train_arg: bool, train_task_embedding: bool, train_head: bool):
@@ -89,13 +92,15 @@ def main(train_right=True, train_left=False):
             parser, data_path, lang_idx=0, direction=0)
         # train_dl, test_dl, val_dl, train_ds, test_ds, val_ds
         # TODO: why return a tuple? turn this into a dict
-        model_wrapped = ModelWrapped(parser, learned_params, ckpt=Checkpoint_saver,model=model,nbatches_train=len(train_dl))
-        trainer.fit(model_wrapped, train_dataloaders=train_dl, val_dataloaders=test_dl)
     if train_left:
         train_dl, test_dl, val_dl, *the_datasets = get_dataset_for_spatial_realtions(
             parser, data_path,  lang_idx=0,    direction=1)
-        model_wrapped = ModelWrapped(parser, learned_params, ckpt=Checkpoint_saver)
-        trainer.fit(model_wrapped, train_dataloaders=train_dl, val_dataloaders=test_dl)
+    model_wrapped = ModelWrapped(
+        parser, learned_params, ckpt=Checkpoint_saver, model=model, nbatches_train=len(train_dl))
+    wandb_logger.watch(model=model_wrapped,log='all')
+    # log all model topology and grads tp the website
+    trainer.fit(model_wrapped, train_dataloaders=train_dl,
+                val_dataloaders=test_dl)
 
 
 main(True, False)

yonathan/Recognicion/code/supp/loss_and_accuracy.py
@@ -40,7 +40,7 @@ def multi_label_accuracy_base(outs: Union[SimpleNamespace,object], samples: obje
         label_task = samples.label_task.squeeze(-1)
         # Take the single original label for each sample
         task_accuracy = (
-            (predictions_by_correct_task == label_task).float()).sum()
+            (predictions_by_correct_task == label_task).float()).sum() / cur_batch_size
         # task_accuracy here is the number of correct predictions
         # Compare the number of matches and normalize by the batch size*num_outputs.
     else:
@@ -73,7 +73,10 @@ def multi_label_accuracy_base(outs: Union[SimpleNamespace,object], samples: obje
 
 def multi_label_accuracy(outs: object, samples: object):
     """
+    .. note:: Deprecated in 11_0
+    ### Marked for deprecation ###
     return the task accuracy mean over all samples.
+    
     Args:
         outs: The model outs.
         samples: The samples.
@@ -84,6 +87,7 @@ def multi_label_accuracy(outs: object, samples: object):
     preds, task_accuracy = multi_label_accuracy_base(outs, samples)
     # per single example
     avg_task_accuracy = task_accuracy.mean()
+
     return preds, avg_task_accuracy
 #
 
@@ -208,6 +212,8 @@ def UnifiedCriterion(opts: argparse.ArgumentParser, inputs: list[torch.Tensor], 
 
 def accuracy(parser: nn.Module, test_data_loader: DataLoader) -> float:
     """
+    *** Deprecated ***
+    Since that function accumulates the accuracy over the whole dataset, it is not suitable for large datasets. We will use the `wandb.log` function instead.
     Args:
         model: The model options.
         test_data_loader: The test data.