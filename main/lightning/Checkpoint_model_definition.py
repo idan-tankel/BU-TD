update normalization blocks, code bugs;imports fixed

main/lightning/Checkpoint_model_definition.py
@@ -3,10 +3,9 @@ import argparse
 import logging
 import os
 import numpy as np
-from supplmentery.general_functions import preprocess
 import torch.optim as optim
 import torch
-from supplmentery.general_functions import create_optimizer_and_sched
+from supplmentery.training_functions import create_optimizer_and_sched
 from pytorch_lightning import LightningModule
 import os.path
 import sys
@@ -154,7 +153,9 @@ class ModelWrapped(LightningModule):
     def accuracy_dl(self, dl):
         acc = 0.0
         for inputs in dl:
-            inputs = preprocess(inputs)
+            dev = torch.device(
+    "cuda") if torch.cuda.is_available() else torch.device("cpu")
+            inputs = [item.to(dev) for item in inputs]
             outs = self.model(inputs)
             samples = self.opts.inputs_to_struct(inputs)
             outs = self.model.outs_to_struct(outs)

main/supplmentery/blocks.py
@@ -4,6 +4,7 @@ import torch._C
 import numpy as np
 from supplmentery.general_functions import flag_to_task, conv3x3, conv1x1, get_laterals, conv3x3up
 from supplmentery.FlagAt import FlagAt
+from .batch_norm import BatchNorm
 import argparse
 
 
@@ -219,11 +220,11 @@ class BUInitialBlock(nn.Module):
         """
         super(BUInitialBlock, self).__init__()
         self.filters = opts.nfilters[0]
-        self.norm_layer = opts.norm_fun
+        self.norm_layer = BatchNorm
         self.activation_func = opts.activation_fun
         self.orig_relus = opts.orig_relus
         self.ntasks = opts.ntasks
-        self.conv1 = nn.Sequential(shared.conv1, opts.norm_fun(self.filters, opts.ntasks),
+        self.conv1 = nn.Sequential(shared.conv1, self.norm_layer(self.filters, opts.ntasks),
                                    self.activation_func())  # The initial block downsampling from RGB.
         self.bot_lat = SideAndCombShared(shared.bot_lat, self.norm_layer, self.activation_func, self.orig_relus,
                                          self.ntasks)  # Skip connection from the TD initial embedding.

main/supplmentery/models.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 from .heads import *
-
+from .blocks import BasicBlockBUShared,SideAndCombSharedBase,BUInitialBlock
+from .batch_norm import BatchNorm
 from types import SimpleNamespace
 import numpy as np
 
@@ -121,11 +122,11 @@ class TDModel(nn.Module):
 class BUStream(nn.Module):
     def __init__(self, opts: argparse, shared: nn.Module, is_bu2: bool) -> None:
         super(BUStream, self).__init__()
-        self.block = opts.bu_block_type
+        self.block = BasicBlockBUShared
         self.inshapes = opts.bu_inshapes
         self.ntasks = opts.ntasks
         self.task_embedding = [[] for _ in range(self.ntasks)]
-        self.norm_layer = opts.norm_fun
+        self.norm_layer = BatchNorm
         self.activation_fun = opts.activation_fun
         self.model_flag = opts.model_flag
         self.inshapes = shared.inshapes_one_list
@@ -198,7 +199,7 @@ class BUStreamShared(nn.Module):
         self.activation_fun = opts.activation_fun
         self.use_lateral = opts.use_lateral_tdbu
         self.use_bu1_flag = opts.use_bu1_flag
-        self.block = opts.bu_shared_block_type
+        self.block = BasicBlockBUShared
         stride = opts.strides[0]
         filters = opts.nfilters[0]
         inplanes = opts.inshape[0]
@@ -349,20 +350,26 @@ class BUTDModel(nn.Module):
 
 
 class BUTDModelShared(BUTDModel):
-    def __init__(self, opts: argparse) -> None:
+    def __init__(self, config,**kwargs) -> None:
         """
-        :param opts: opts to create the model according to.
+        :param opts: opts to create the model according to. Loaded from the yaml config in the new version
         """
-        super(BUTDModelShared, self).__init__()
+        super(BUTDModelShared, self).__init__(**kwargs)
+        try:
+            opts = config.Models
+        except AttributeError:
+            opts = config
         self.ntasks = opts.ntasks
         self.use_lateral_butd = opts.use_lateral_butd
         self.use_lateral_tdbu = opts.use_lateral_tdbu
-        self.inputs_to_struct = opts.inputs_to_struct
+        # self.inputs_to_struct = opts.inputs_to_struct
         self.task_embedding = [[] for _ in range(self.ntasks)]  # Container to store the task embedding.
         self.transfer_learning = [[] for _ in range(self.ntasks)]
-        self.model_flag = opts.model_flag  # The model type
-        self.use_bu1_loss = opts.use_bu1_loss  # Whether to use the Occurrence loss.
-        self.use_td_flag = opts.use_td_flag
+        self.model_flag = config.RunningSpecs.Flag  # The model type
+        # losses options
+        
+        self.use_bu1_loss = config.Losses.use_bu1_loss# Whether to use the Occurrence loss.
+        self.use_td_flag = config.use_td_flag
         if self.use_bu1_loss:
             self.occhead = OccurrenceHead(opts)
         bu_shared = BUStreamShared(opts)  # The shared part between BU1, BU2.

yonathan/training_cifar10/code/supp/models.py
@@ -200,7 +200,7 @@ class BUStreamShared(nn.Module):
         self.activation_fun = opts.activation_fun
         self.use_lateral = opts.use_lateral_tdbu
         self.use_bu1_flag = opts.use_bu1_flag
-        self.block = opts.bu_shared_block_type
+        self.block = BasicBlockBUShared
         stride = opts.strides[0]
         filters = opts.nfilters[0]
         inplanes = opts.inshape[0]